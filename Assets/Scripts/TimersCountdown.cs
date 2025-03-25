8472a712c209092dfa22c4b271a2dabbad087a4d
AstroBoy2013 <joshuakim5477@gmail.com>
2025-03-25 18:47:02 -0400

    116

--- Assets/Scripts/TimersCountdown.cs
+++ Assets/Scripts/TimersCountdown.cs
@@ -1,11 +1,14 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimersCountdown : MonoBehaviour
 {
     public Text lapTime;
+    public CodeyMove cm;
     public Text startCountdown;
 
     public float totalLapTime;
@@ -18,7 +21,28 @@ public class TimersCountdown : MonoBehaviour
         totalLapTime -= Time.deltaTime;
         totalCountdownTime -= Time.deltaTime;
 
-        lapTime.text = totalLapTime.ToString();
+        lapTime.text = Mathf.Round(totalLapTime).ToString();
+        startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
+        if (totalCountdownTime > 0) 
+        { 
+        totalCountdownTime -= Time.deltaTime;
         startCountdown.text = totalCountdownTime.ToString();
+            cm.Speed = 0;
+        }
+
+        if (totalCountdownTime <= 0)
+        {
+            startCountdown.text = "";
+            totalLapTime -= Time.deltaTime;
+            lapTime.text = totalLapTime.ToString();
+            cm.Speed = 60;
+        }
+        if(totalLapTime <= 0)
+        {
+            print("Time is up!");
+        }
+
+            
     }
+    
 }
